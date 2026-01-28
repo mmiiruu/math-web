42a1d8042d5881865ee45fb7af9a06f930105cc7
TDem0nT <bhuwadit.ketngam@gmail.com>
2026-01-28 12:50:59 +0700

    test

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <div className="home-page h-screen font-sans  flex-col flex justify-center items-center ">
-      <h1 className="font-medium text-[150px]">คณิตคิดเร็ว</h1>
+      <h1 className="font-medium text-[150px]">คณิตคิดเร็</h1>
       <Link href="/level-selection">
         <button className=" w-[300px] h-[75px] text-4xl">เริ่ม</button>
       </Link>
